711d9808ae665f0dfa63e6d2f4343a0948190a95
Leilani Leach <leilanileach@yahoo.com>
2019-01-18 17:01:04 -0800

    display specialties as radio button form options

--- src/main.js
+++ src/main.js
@@ -14,23 +14,27 @@ $(document).ready(function () {
     specialtyPromise.then(function (response) {
       let body = JSON.parse(response);
       for (let i = 0; i < body.data.length; i++) {
-        $("#specialties").append(body.data[i].name);
+        $("#specialties").append(`<input type="radio" name="${body.data[i].name}" value="{body.data[i].uid}">
+        <label for="${body.data[i].name}">
+          name="${body.data[i].name}"
+        </label>`);
       }
     })
-    $("#submitButton").click(function (event) {
-      event.preventDefault();
-      let nameQuery = $("#doctorName").val();
-      let condition = "";
-      let userSearch = new Search(nameQuery, condition);
-      let promise = userSearch.GetDoctors();
-      promise.then(function (response) {
-        let body = JSON.parse(response);
-        for (let i = 0; i < body.data.length; i++) {
-          $("#available").append(`<li>${body.data[i].practices[0].accepts_new_patients}</li>`);
-          $("#name").append(`<li>${body.data[i].practices[0].name}
+  })
+  $("#submitButton").click(function (event) {
+    event.preventDefault();
+    let nameQuery = $("#doctorName").val();
+    let condition = "";
+    let userSearch = new Search(nameQuery, condition);
+    let promise = userSearch.GetDoctors();
+    promise.then(function (response) {
+      let body = JSON.parse(response);
+      for (let i = 0; i < body.data.length; i++) {
+        $("#available").append(`<li>${body.data[i].practices[0].accepts_new_patients}</li>`);
+        $("#name").append(`<li>${body.data[i].practices[0].name}
         </li>`);
-          $("#phone").append(`<li>${body.data[i].practices[0].phones[0].number}</li>`);
-          $("#address").append(`<li>${body.data[i].practices[0].visit_address.street},
+        $("#phone").append(`<li>${body.data[i].practices[0].phones[0].number}</li>`);
+        $("#address").append(`<li>${body.data[i].practices[0].visit_address.street},
 
         <span>${body.data[i].practices[0].visit_address.street2},</span>
 
@@ -38,8 +42,8 @@ $(document).ready(function () {
 
         <span>${body.data[i].practices[0].visit_address.state}</span>
         </li>`);
-        }
-      })
+      }
     })
   })
 })
+
